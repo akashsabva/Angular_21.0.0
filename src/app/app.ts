42dc90115497708554a698410ceb42f92085ec2f
Akash Sabva <akash.sabva@streebo.com>
2026-07-10 14:17:25 +0530

    Iterpolation

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('angular-tutorial');
+  name = "Sky";
+  getUser() {
+    return "Return from Function";
+  }
+  summary = signal("This is Summary Signal");
 }
