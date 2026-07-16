3fa12ca6d5b658647740891ac0906a435f1dbf77
Akash Sabva <akash.sabva@streebo.com>
2026-07-16 21:02:09 +0530

    Router : RouterLinkActive & create Header component for better UI

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
